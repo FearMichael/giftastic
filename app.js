45d0b84bfed74438c50de79bd9b13feb81212331
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-01-23 18:26:37 -0500

    Finalized!!

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
 var gifSearchButton = $("#makeGifs");
-var queryURL;
 var gifZone = $("#gifZone");
 var gifButtons = $(".gifButtons");
-var topics = ["dogs","cats","laughing"]
+var topics = ["dogs","cats","laughing"];
 
-
-function getGifs() {
+function getGifs(queryURL) {
     $.ajax({
         url: queryURL,
         method: "GET",
@@ -14,9 +12,11 @@ function getGifs() {
         for (i=0; i<10; i++) {
             var imgDiv = $("<div>").addClass("col l3 s6");
             var gifImg = $("<img>").addClass("responsive-img");
+            var gifText = $("<p>").addClass("white-text imageCaption");
             gifImg.attr("src", info.data[i].images.fixed_width.url);
             gifImg.attr("still", info.data[i].images.fixed_width_still.url);
             gifImg.attr("move", info.data[i].images.fixed_width.url);
+            gifText.text("Title: " + info.data[i].title + " / " + "Rating: " + info.data[i].rating)
             gifImg.click(function() {
                 var still = $(this).attr("still");
                 var move = $(this).attr("move");
@@ -28,43 +28,57 @@ function getGifs() {
             });
             gifZone.append(imgDiv);
             imgDiv.append(gifImg);
+            imgDiv.append(gifText);
         };
     });
 };
 
+//Justin API Key IzBmA6Dpos0EdfXIav4LTX8PYpp03Nj6
+//My API Key UQlAY3AHnV7hwkRT2LuYZ3yzeoCA9smT
+
 function makeButtons() {
+    var gifSearch = $("#gifSearch").val();
+    if (topics.indexOf(gifSearch.toLowerCase()) == -1 && gifSearch != "") {
+        topics.push(gifSearch)
+    };
     gifButtons.empty();
-    topics.forEach(function(elem) {
-        var buttons =  $("<button>").addClass("btn btn-small smallwhitebutton").text(elem);
-        buttons.attr("data", elem.trim());
-        //create click function for that button
-        buttons.click(function() {
-            gifZone.empty();
-            var searchItem = $(this).attr("data");
-            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=UQlAY3AHnV7hwkRT2LuYZ3yzeoCA9smT";
-            getGifs();
-        });
+        topics.forEach(function(elem) {
+            var buttons = $("<button>").addClass("btn btn-small smallwhitebutton").text(elem);
+            buttons.attr("data", elem.trim());
+            //create click function for that button
+            buttons.click(function() {
+                gifZone.empty();
+                var searchItem = $(this).attr("data");
+                let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=IzBmA6Dpos0EdfXIav4LTX8PYpp03Nj6";
+                getGifs(queryURL);
+            });
         gifButtons.append(buttons);
     });
+
 };
 
 gifSearchButton.click(function(event) {
-    var gifSearch = $("#gifSearch").val();
-    topics.push(gifSearch)
-    // $("#gifSearch").clear(); 
-    //create button in gifButtons
     makeButtons();
-    getGifs();
+    gifZone.empty();
+    let searchItem = $("#gifSearch").val().trim().toLowerCase();
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=IzBmA6Dpos0EdfXIav4LTX8PYpp03Nj6";
+    getGifs(queryURL);
 });
-makeButtons();
+
 
 $("input").on("keypress", function(event) {
     if (event.charCode == 13) {
-        event.preventDefault();
         makeButtons();
+        gifZone.empty();
+        event.preventDefault();
     };
+    let searchItem = $("#gifSearch").val().trim().toLowerCase();
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=IzBmA6Dpos0EdfXIav4LTX8PYpp03Nj6";
+    getGifs(queryURL);
 });
 
 $("#clearGifs").click(function() {
     gifZone.empty();
 });
+
+makeButtons();
